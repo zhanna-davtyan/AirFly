34ad480c508072d0c6d670343d4e237bc692bce2
Kristian Klassen <kristian.klassen@arvato-scs.com>
2024-10-28 12:43:57 +0100

    chore(setup): added translationService

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,27 +2,45 @@ import {ApplicationConfig, importProvidersFrom, isDevMode} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideAnimations} from "@angular/platform-browser/animations";
-import {provideHttpClient} from "@angular/common/http";
+import {BrowserAnimationsModule, provideAnimations} from "@angular/platform-browser/animations";
+import {HttpClient, HttpClientModule } from "@angular/common/http";
 import {ConfirmationService, MessageService} from "primeng/api";
 import {DialogService} from "primeng/dynamicdialog";
 import { provideServiceWorker } from '@angular/service-worker';
 import {CommonModule} from "@angular/common";
 import {BrowserModule} from "@angular/platform-browser";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
+}
+
+export const provideTranslation = () => ({
+  defaultLanguage: 'en',
+  loader: {
+    provide: TranslateLoader,
+    useFactory: HttpLoaderFactory,
+    deps: [HttpClient],
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    importProvidersFrom(BrowserModule),
+    importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(CommonModule),
+    importProvidersFrom(HttpClientModule),
     provideAnimations(),
-    provideHttpClient(),
+    importProvidersFrom(TranslateModule.forRoot(provideTranslation())),
     MessageService,
     DialogService,
     ConfirmationService,
-    importProvidersFrom(CommonModule),
-    importProvidersFrom(BrowserModule),
     provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
-    })
-]
+    }),
+    importProvidersFrom(TranslateModule.forRoot(provideTranslation()))
+  ]
 };
