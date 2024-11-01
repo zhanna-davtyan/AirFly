3526452608c05eee580199e16722b9778ce7f8bb
zhanna-davtyan <zhanna.davtyan@edu.fhdw.de>
2024-11-01 10:06:42 +0100

    Sprachenauswahl in Sidebar globe icon gebracht

--- frontend/src/app/menu/menu.component.ts
+++ frontend/src/app/menu/menu.component.ts
@@ -1,11 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { RouterModule, ROUTES, Routes } from '@angular/router';
 import { LandingPageComponent } from '../landing-page/landing-page.component';
 import { FlightSearchComponent } from '../flight/flight-search/flight-search.component';
 import { MenuSidebarService } from './menu-sidebar.service';
-
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { TranslateService } from '@ngx-translate/core';
+import { SelectItem } from 'primeng/api';
+import { switchMap, take } from 'rxjs';
+import { TooltipModule } from 'primeng/tooltip';
+import { OverlayPanel, OverlayPanelModule } from 'primeng/overlaypanel';
+import { TranslationChangeEvent } from '@ngx-translate/core'; 
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
@@ -14,15 +27,29 @@ const routes: Routes = [
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule, SidebarModule, RouterModule],
+  imports: [
+    CommonModule,
+    SidebarModule,
+    RouterModule,
+    FormsModule,
+    DropdownModule,
+    TooltipModule,
+    OverlayPanelModule,
+  ],
   templateUrl: './menu.component.html',
-
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./menu.component.css'],
 })
-
 export class MenuComponent implements OnInit {
+  @ViewChild('op') op!: OverlayPanel;
+
   constructor(private menusidebarService: MenuSidebarService) {}
   visibleSidebar: boolean = false;
+  protected selectedLanguage: string | null = localStorage.getItem('lng');
+  languageOptions: SelectItem[] = [];
+
+  private readonly availableLanguages = ['de', 'en'];
+  translateService = inject(TranslateService);
 
   items = [
     { label: 'Home', icon: 'pi pi-home', routerLink: '/' },
@@ -38,6 +65,42 @@ export class MenuComponent implements OnInit {
     this.menusidebarService.isSidebarVisible$.subscribe((value) => {
       this.visibleSidebar = value;
     });
+    this.translateService.addLangs(this.availableLanguages);
+    if (this.selectedLanguage == null) {
+      this.selectedLanguage = 'en'; // Default
+      localStorage.setItem('lng', this.selectedLanguage);
+    }
+    this.translateService.use(this.selectedLanguage);
+    this.updateTranslations();
+  }
+
+  protected onChangeLanguage(event: any) {
+    const newLanguage = event.value; 
+    localStorage.setItem('lng', newLanguage);
+  
+    this.translateService.use(newLanguage)
+      .pipe(
+        switchMap(() => this.translateService.get(['base.language.en', 'base.language.de'])), // Hole Übersetzungen nach Sprachwechsel
+        take(1)
+      )
+      .subscribe((translations) => {
+        this.languageOptions = this.availableLanguages.map((option) => ({
+          value: option,
+          label: translations['base.language.' + option],
+        }));
+      });
+  }
+
+  private updateTranslations() {
+    this.translateService
+      .get(['base.language.en', 'base.language.de'])
+      .pipe(take(1))
+      .subscribe((translations) => {
+        this.languageOptions = this.availableLanguages.map((option) => ({
+          value: option,
+          label: translations['base.language.' + option],
+        }));
+      });
   }
 
   toggleSidebar() {
