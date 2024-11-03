9450775e8d7558698dd577e29872f486007201be
zhanna-davtyan <zhanna.davtyan@edu.fhdw.de>
2024-11-03 12:05:50 +0100

    Carousel Implementierung

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
 import {FlightSearchComponent} from "../flight/flight-search/flight-search.component";
 import {CardModule} from "primeng/card";
+import { CarouselModule } from 'primeng/carousel';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
     imports: [
-        FlightSearchComponent,
-        CardModule
-    ],
+    FlightSearchComponent,
+    CardModule,
+    CarouselModule,
+    TranslateModule,
+],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
+  destinations = [
+    { 
+        image: 'assets/img/Tokio.jpg', 
+        name: 'Tokyo', 
+        price: 1300 
+    },
+    { 
+      image: 'assets/img/Paris.jpg', 
+      name: 'Paris', 
+        price: 290 
+    },
+    { 
+      image: 'assets/img/Malediven.jpg', 
+      name: 'Malediven', 
+        price: 537 
+    },
 
+];
 }
